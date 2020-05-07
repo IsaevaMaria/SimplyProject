d23cd7d02286140853030b40b1974c763a6d028d
IsaevaMaria <isaevam2015@gmail.com>
2020-05-07 20:36:52 +0300

    update

--- main.py
+++ main.py
@@ -7,4 +7,5 @@ app = Flask(__name__)
 def index():
     return render_template('index.html', title='Мой город - Кострома')
 
-app.run(port=8080, host='127.0.0.1')
+if __name__ == '__main__':
+    app.run()
