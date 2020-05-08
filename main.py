51e4407da1c55bc3eada7a5a4efbff0bc43ea526
IsaevaMaria <isaevam2015@gmail.com>
2020-05-08 11:19:05 +0300

    update

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from flask import Flask, render_template
 
+
 app = Flask(__name__)
 
 
@@ -7,3 +8,5 @@ app = Flask(__name__)
 def index():
     return render_template('index.html', title='Мой город - Кострома')
 
+if __name__=='__main__':
+    app.run(debug=True)
